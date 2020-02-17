75b5a9de42754ebf8500347d9bef3282b17d663a
renzhongrui <zrzoffice@163.com>
2020-02-18 00:16:43 +0800

    cout的使用

--- 002_c++_const/002_c++_const/002_c++_const.cpp
+++ 002_c++_const/002_c++_const/002_c++_const.cpp
@@ -3,10 +3,16 @@
 
 #include "stdafx.h"
 
-
+// cout要引入iostream并且使用命名空间namespace
+#include <iostream>
+using namespace std;
 int main()
 {
-	printf("%s\n","hello");
+	const int a = 0xFe;// 254
+	const int b = 017;// 15
+	cout << a<<endl;
+	std:cout << "hell" << endl;
+	printf("b = %d\n", b);
     return 0;
 } 
 
---
+++ 002_c++_const/README.md
@@ -0,0 +1,5 @@
+### 整数常量
+0开头表示8进制，如const int a = 007;
+0x开头表示16进制，如const int a = 0xfe 或者0XFe;
+### 布尔常量
+true或者false
